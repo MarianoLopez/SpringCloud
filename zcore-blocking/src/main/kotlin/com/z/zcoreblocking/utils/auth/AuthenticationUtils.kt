8ff254810e00391b809d5af4570bb98c2b749891
m_villa@hotmail.com <m_villa@hotmail.com>
2020-05-02 20:37:20 -0300

    add null check for getContext

--- zcore-blocking/src/main/kotlin/com/z/zcoreblocking/utils/auth/AuthenticationUtils.kt
+++ zcore-blocking/src/main/kotlin/com/z/zcoreblocking/utils/auth/AuthenticationUtils.kt
@@ -1,10 +1,12 @@
 package com.z.zcoreblocking.utils.auth
 
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
 import org.springframework.security.core.Authentication
 import org.springframework.security.core.context.SecurityContextHolder
 
 object AuthenticationUtils {
     fun getCurrentAuthentication(): Authentication {
-        return SecurityContextHolder.getContext().authentication
+        return SecurityContextHolder.getContext()?.authentication
+                ?: UsernamePasswordAuthenticationToken("anonymous","")
     }
 }
